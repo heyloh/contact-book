cffcc233fa0b5ad38ad98dec9807c45ba93890b1
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-04 20:52:22 -0300

    Add README.md

---
+++ frontend/src/pages/SignInUp/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import './styles.css';
+
+function SignInUp() {
+  return(
+    <>
+      <Header />
+      <p>oi</p>
+    </>
+  );
+}
+
+export default SignInUp;
